67332f454c6ecfec7ba14a2d0e8d8b39fa51ca7c
jones-py <jones03chimfwembe@gmail.com>
2020-11-21 03:16:22 +0000

    header and footer completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
